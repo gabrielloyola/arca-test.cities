c7c3f9b164e298b5060bc3b3ade9c03bf2d4fb72
gabrielloyola <gabrielfrml@gmail.com>
2021-04-22 05:09:56 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pr = State.create!(name: 'Paraná', initials: 'PR')
+sc = State.create!(name: 'Santa Catarina', initials: 'SC')
+rs = State.create!(name: 'Rio Grande do Sul', initials: 'RS')
+
+%w[Curitiba Londrina Maringá Guaratuba Ponta\ Grossa].each do |city_name|
+  City.create!(name: city_name, state: pr)
+end
+
+%w[Joinville Florianópolis Itajaí Blumenau].each do |city_name|
+  City.create!(name: city_name, state: sc)
+end
+
+%w[Porto\ Alegre Pelotas Canoas Santa\ Maria].each do |city_name|
+  City.create!(name: city_name, state: rs)
+end
